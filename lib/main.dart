3444b251eca64e5a9637f54d1e2844b1acfb9df0
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-08 22:32:45 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
+import 'chewie_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'videos/IntroVideo.mp4',
+            ),
+            looping: true,
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.network(
+              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+            ),
+          ),
+          ChewieListItem(
+            // This URL doesn't exist - will display an error
+            videoPlayerController: VideoPlayerController.network(
+              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/error.mp4',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
